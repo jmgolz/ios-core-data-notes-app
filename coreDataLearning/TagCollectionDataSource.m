1904ea324534ba8850e4cc850c5a6ea5fbe29668
= <=>
2016-03-13 13:46:22 -0400

    Bug fix - added call to deqeue cell with reuse identifier.

--- coreDataLearning/TagCollectionDataSource.m
+++ coreDataLearning/TagCollectionDataSource.m
@@ -12,9 +12,8 @@
 @implementation TagCollectionDataSource
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     
-    TagsCollectionViewCell *cell = [[TagsCollectionViewCell alloc] init];
+    TagsCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TagItemCell" forIndexPath:indexPath];
     [cell setBackgroundColor:[UIColor colorWithRed:0.25f green:0.5f blue:0.25f alpha:1.0f]];
-    
     return cell;
 }
 
